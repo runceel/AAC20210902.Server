ccaba1be4d1cbbc7913c7c3b139c5c031b29100c
Kazuki Ota <kaota@microsoft.com>
2021-09-01 13:16:32 +0900

    change api

--- AAC20210902.Server/Function1.cs
+++ AAC20210902.Server/Function1.cs
@@ -14,22 +14,12 @@ namespace AAC20210902.Server
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            var user = req.GetStaticWebAppUserInfo();
+            return new OkObjectResult(user.Identity.Name);
         }
     }
 }
